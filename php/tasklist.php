99da5dc544763e45c54ff8d0ac07c809f2b6bab1
RyanRen2023 <renxihai@gmail.com>
2024-03-26 16:39:33 -0400

    PHP test

--- php/tasklist.php
+++ php/tasklist.php
@@ -1,5 +1,17 @@
 <?php
-require('db.php');
-require('fetching_tasklist.php');
-$tasks = getTasks($conn);
+    require('db.php');
+    require('fetching_tasklist.php');
+    $sql = "SELECT Username, Password, Email FROM Users";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+        echo "USERNAME: " . $row["Username"]. " - PASSWORD: " . $row["Password"]. " - EMAIL" . $row["Email"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+
 ?>
