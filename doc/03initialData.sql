45b18baba6288028875f65c2862931f976d13c4b
Gloria <gloria_w1985@hotmail.com>
2024-04-04 11:35:50 -0400

    Update 03initialData.sql

--- doc/03initialData.sql
+++ doc/03initialData.sql
@@ -1,28 +1,38 @@
+
+use task_management_db;
+delete from Tasks;
+delete from TaskLists;
+delete from TaskStatuses;
+delete from TaskCategories;
+delete from Users;
+
+
+
 -- Active: 1711483040803@@127.0.0.1@3306@task_management_db
 -- create test data for Users 
-INSERT INTO Users (Username, Password, Email, CreatedDate, UpdatedDate) VALUES
-('user1', 'password1', 'user1@example.com', NOW(), NOW()),
-('user2', 'password2', 'user2@example.com', NOW(), NOW()),
-('user3', 'password3', 'user3@example.com', NOW(), NOW());
-('user4', 'Password666@', 'user4@example.com', NOW(), NOW());
+INSERT INTO Users (userid,Username, Password, Email, CreatedDate, UpdatedDate) VALUES
+(1,'user1', 'password1', 'user1@example.com', NOW(), NOW()),
+(2,'user2', 'password2', 'user2@example.com', NOW(), NOW()),
+(3,'user3', 'password3', 'user3@example.com', NOW(), NOW()),
+(4,'user4', 'Password666@', 'user4@example.com', NOW(), NOW());
 -- create test data for TaskLists
-INSERT INTO TaskLists (ListName, UserID, IsPersonalList, CreatedDate, UpdatedDate) VALUES
-('Personal List 1', 1, true, NOW(), NOW()),
-('Personal List 2', 1, true, NOW(), NOW()),
-('Shared List 1', 2, false, NOW(), NOW()),
-('Shared List 2', 3, false, NOW(), NOW());
+INSERT INTO TaskLists (listid,ListName, UserID, IsPersonalList, CreatedDate, UpdatedDate) VALUES
+(1,'Personal List 1', 1, true, NOW(), NOW()),
+(2,'Personal List 2', 1, true, NOW(), NOW()),
+(3,'Shared List 1', 2, false, NOW(), NOW()),
+(4,'Shared List 2', 3, false, NOW(), NOW());
 
 -- create test data for TaskStatuses
-INSERT INTO TaskStatuses (StatusName, CreatedDate, UpdatedDate) VALUES
-('To Do', NOW(), NOW()),
-('In Progress', NOW(), NOW()),
-('Completed', NOW(), NOW());
+INSERT INTO TaskStatuses (statusid,StatusName, CreatedDate, UpdatedDate) VALUES
+(1,'To Do', NOW(), NOW()),
+(2,'In Progress', NOW(), NOW()),
+(3,'Completed', NOW(), NOW());
 
 -- create test data for TaskCategories
-INSERT INTO TaskCategories (CategoryName, CreatedDate, UpdatedDate) VALUES
-('Category 1', NOW(), NOW()),
-('Category 2', NOW(), NOW()),
-('Category 3', NOW(), NOW());
+INSERT INTO TaskCategories (CategoryId,CategoryName, CreatedDate, UpdatedDate) VALUES
+(1,'Category 1', NOW(), NOW()),
+(2,'Category 2', NOW(), NOW()),
+(3,'Category 3', NOW(), NOW());
 
 -- create test data for Tasks
 INSERT INTO Tasks (Title, Description, DueDate, Priority, StatusID, CategoryID, ListID, AssignedToUserID, CreatedByUserID, CreatedDate, UpdatedDate) VALUES
