78c5e9c52694fdb0fba1902a95c3006b28df36bb
RyanRen2023 <renxihai@gmail.com>
2024-04-07 01:48:05 -0400

    modify the task edit function

--- html/tasklist.php
+++ html/tasklist.php
@@ -53,24 +53,24 @@ $doneTasks = $taskDao->getTasksByStatusAndUser(3, $userID);
 
   <!-- <div class="top-nav-2">
     <!-- Button to open the modal -->
-    <!--<button id="searchBtn" class="search-btn" class="top-right">Search</button>-->
-    <!-- <a href="../html/search.php" class="top-right">Search</a>
+  <!--<button id="searchBtn" class="search-btn" class="top-right">Search</button>-->
+  <!-- <a href="../html/search.php" class="top-right">Search</a>
     <a href="#add" class="top-right">Add</a> -->
-  <!-- </div> --> 
+  <!-- </div> -->
 
   <div class="top-nav-2 ">
     <?php
-        echo '<h1 id="username" class="welcome-h1">' . $username . '</h1>';
-        echo '<h2 id="useremail" class="welcome-p">' . $email . '</h2>';
-    ?>   
+    echo '<h1 id="username" class="welcome-h1">' . $username . '</h1>';
+    echo '<h2 id="useremail" class="welcome-p">' . $email . '</h2>';
+    ?>
   </div>
 
   <!-- add task section -->
-  <div class= "button-container">
-  <button class="addtask-btn" id="addTaskButton">Add Task</button>
-  <button class="addtask-btn" id="searchButton" onclick="goToSearch()">Search</button>
+  <div class="button-container">
+    <button class="addtask-btn" id="addTaskButton">Add Task</button>
+    <button class="addtask-btn" id="searchButton" onclick="goToSearch()">Search</button>
   </div>
-  
+
 
 
 
@@ -115,36 +115,47 @@ $doneTasks = $taskDao->getTasksByStatusAndUser(3, $userID);
     <button class="delete-button">Delete</button>
   </div>
 
-  <!-- Edit task status -->
-  <div class="modal edit-task-status" id="edit-task-status">
-    <section class="modal-content task-status-content">
-      <h2 id="task-title">Task Title</h2>
-      <div>
-        <input type="hidden" name="taskid" id="taskId" value="">
-        <input type="hidden" name="default-status" id="default-status" value="">
+  <!-- Edit task status develop by Xihai Ren-->
+  <div class="modal" id="edit-task-status">
+    <section class="modal-content">
+      <h2 id="task-title">Edit Task</h2>
+      <div class="taskForm">
+        <form action="" id="editTaskForm">
+          <label for="taskId">taskId:</label>
+          <input type="text" name="taskId" id="taskId" value="" readonly><br>
 
-        <ul class="task-stauts-list">
-          <li>
-            <input type="radio" name="status" id="status1" value="1">
-            <label for="status1">To Do</label>
-          </li>
-          <li>
-            <input type="radio" name="status" id="status2" value="2">
-            <label for="status2">In Progress</label>
-          </li>
-          <li>
-            <input type="radio" name="status" id="status3" value="3">
-            <label for="status3">Completed</label>
-          </li>
-        </ul>
-        <button type="button" onclick="updateStatusEvent()" class="edit-button">Confirm</button>&nbsp;
-        <button type="button" onclick="closeEditModal()" class="delete-button">Close</button>
+          <label for="title">Title:</label>
+          <input type="text" id="title" name="title" required><br>
+
+          <label for="description">Description:</label>
+          <textarea id="description" name="description" required></textarea><br>
+
+          <label for="dueDate">Due Date:</label>
+          <input type="date" id="dueDate" name="dueDate" required><br>
+
+          <label for="priority">Priority:</label>
+          <select id="priority" name="priority">
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+          </select><br>
+          <label for="status">Status:</label>
+          <select id="status" name="status">
+            <option value="1">To Do</option>
+            <option value="2">Doing</option>
+            <option value="3">Done</option>
+          </select><br>
+          <button type="button" onclick="updateTaskFormSubmit()" class="submit">Update</button>&nbsp;
+          <button type="button" onclick="closeEditModal()" class="reset">Close</button>
+
+          <input type="hidden" name="userId" value="<?php echo $userID?>">
+        </form>
       </div>
     </section>
   </div>
 
   <!-- delete task by ID -->
-  <div class="modal delete-task" id="delete-task">
+  <div class="modal" id="delete-task">
     <section class="modal-content task-status-content">
       <h2 id="task-title">Delete Task</h2>
       <p id="delete-confirm-message"> Are you sure to delete Task: Task 1?</p>
@@ -156,11 +167,11 @@ $doneTasks = $taskDao->getTasksByStatusAndUser(3, $userID);
     </section>
   </div>
 
-  <!-- Add task form -->
-  <div class="modal addTaskForm" id="add-task">
+  <!-- Add task form develop by Elton-->
+  <div class="modal" id="add-task">
     <section class="modal-content addTaskForm-content">
       <h2 id="task-title">Add Task</h2>
-      <div id="addTaskForm">
+      <div id="addTaskForm" class=".taskForm">
         <form action="../php/AddTask.php" method="post">
           <label for="title">Title:</label>
           <input type="text" id="title" name="title" required><br>
@@ -186,7 +197,7 @@ $doneTasks = $taskDao->getTasksByStatusAndUser(3, $userID);
           </select><br>
 
           <input class="addtask-btn" type="submit" value="Add Task">
-          <input class="reset-btn" type="reset" id ="cancelButton" value="Cancel">
+          <input class="reset-btn" type="reset" id="cancelButton" value="Cancel">
         </form>
       </div>
     </section>
